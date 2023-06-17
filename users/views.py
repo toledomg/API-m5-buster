bb6a7069101efff73c1374632e0fe9f27c53210f
toledomg <toledomg@yahoo.com.br>
2023-06-17 15:35:45 -0300

    :sparkles: feat: app Movies e Crud implementados

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView, Request, Response, status
-
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 from users.models import User
-from .utils import ResponseMethods
+from utils import ResponseMethods
 from .serializers import UserSerializer
+from .permissions import IsUserOwnerOrAdmin
 
 
 class UserView(APIView):
@@ -15,8 +17,27 @@ class UserView(APIView):
         serializer.save()
 
         return ResponseMethods.response_success(201, serializer.data)
-        # return Response(serializer.data, status.HTTP_201_CREATED)
 
 
 class UserDetailView(APIView):
-    pass
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsUserOwnerOrAdmin]
+
+    def get(self, req: Request, user_id: int) -> Response:
+        user = get_object_or_404(User, id=user_id)
+        self.check_object_permissions(req, user)
+
+        serializer = UserSerializer(user)
+
+        return ResponseMethods.response_success(200, serializer.data)
+
+    def patch(self, req: Request, user_id: int) -> Response:
+        user = get_object_or_404(User, id=user_id)
+        self.check_object_permissions(req, user)
+
+        serializer = UserSerializer(user, req.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return ResponseMethods.response_success(200, serializer.data)
